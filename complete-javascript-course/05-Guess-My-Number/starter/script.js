a46e6f84e3d00292514a9e1b6782c7b5f0d93d19
Jackimaru96 <jackchenyujie.96@gmail.com>
2021-06-10 11:44:01 +0800

    Project #1: DOM + Event Handler

--- complete-javascript-course/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course/05-Guess-My-Number/starter/script.js
@@ -18,37 +18,71 @@
 // console.log(document.querySelector('.guess').value);
 
 // Between 1 and 20 (inclusive)
-const correctNum = Math.trunc(Math.random() * 20) + 1;
+let correctNum = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = correctNum;
+let highscore = 0;
+let gameRestart = true;
 
-// Check! button event + DOM manipulation
-document.querySelector('.check').addEventListener('click', function () {
-  const userGuessValue = document.querySelector('.guess').value;
-  console.log(userGuessValue);
-
-  let wrongGuess = true;
-
-  if (!userGuessValue) {
-    document.querySelector('.message').textContext = 'No number input 😑';
-    wrongGuess = false;
-  } else if (userGuessValue === correctNum) {
-    document.querySelector('.message').textContent = 'Correct Number! 😁';
-    wrongGuess = false;
-  } else if (userGuessValue > correctNum) {
-    document.querySelector('.message').textContent = 'Number is too high! ⬆️';
-  } else if (userGuessValue < correctNum) {
-    document.querySelector('.message').textContent = 'Number is too low! ⬇️';
-  }
-
-  if (wrongGuess && score > 0) {
-    score -= 1;
-    document.querySelector('.score').textContent = score;
-  }
-
-  if (score == 0) {
-    document.querySelector('.message').textContent = 'You lose! 😢';
-  }
+document.querySelector('.again').addEventListener('click', function () {
+  gameRestart = true;
+  score = 20;
+  correctNum = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('body').style.backgroundColor = '#000000f7';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
 });
 
+if ((gameRestart = true)) {
+  // Check! button event + DOM manipulation
+  document.querySelector('.check').addEventListener('click', function () {
+    const userGuessValue = Number(document.querySelector('.guess').value);
+    console.log(userGuessValue);
+
+    let wrongGuess = true;
+
+    if (!userGuessValue) {
+      document.querySelector('.message').textContext = 'No number input 😑';
+      wrongGuess = false;
+    }
+    // When player wins
+    else if (userGuessValue === correctNum) {
+      document.querySelector('.message').textContent = 'Correct Number! 😁';
+
+      // Changing CSS Style
+      document.querySelector('body').style.backgroundColor = '#60b347db';
+      document.querySelector('.number').style.width = '30rem';
+      document.querySelector('.number').textContent = correctNum;
+
+      checkHighScore(score, highscore);
+      document.querySelector('.highscore').textContent = highscore;
+      wrongGuess = false;
+      gameRestart = false;
+    }
+    // guess more than number
+    else if (userGuessValue > correctNum) {
+      document.querySelector('.message').textContent = 'Number is too high! ⬆️';
+    }
+    // guess less than number
+    else if (userGuessValue < correctNum) {
+      document.querySelector('.message').textContent = 'Number is too low! ⬇️';
+    }
+
+    if (wrongGuess) {
+      score -= 1;
+      document.querySelector('.score').textContent = score;
+    }
+
+    if (score == 0) {
+      document.querySelector('.message').textContent = 'You lose! 😢';
+      gameRestart = false;
+    }
+  });
+}
+
+function checkHighScore(currentScore, highScore) {
+  if (currentScore > highScore) {
+    highscore = currentScore;
+  }
+}
 // Function needs to be added; this is called EventHandler
